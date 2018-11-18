13235a66d591b4403a48ca916962e43758929e74
mohammedezz5 <45146103+mohammedezz5@users.noreply.github.com>
2018-11-18 20:16:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ if (message.content === '1say') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === '1say') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
